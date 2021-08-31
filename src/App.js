22941ec500ccf82305aa8f606364064cfe54cdd3
Leonardo Dias <leonardodias@ucl.br>
2021-08-31 11:10:29 -0300

    Create basic  Missionns component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './assets/planet.png';
 import Navbar from './components/Navbar';
 import './App.css';
+import Missions from './components/missions/Missions';
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
       <Router>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Navbar />
         </header>
-        <Navbar />
         <Switch>
           <Route exact path="/" />
-          <Route path="/missions" />
+          <Route path="/missions">
+            <Missions />
+          </Route>
           <Route path="/profile" />
         </Switch>
       </Router>
---
+++ src/components/missions/Missions.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Missions = () => {
+  const mission = 'Mission';
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>{mission}</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+    </table>
+  );
+};
+
+export default Missions;
