afc23bbb51ca439b333413bd33cafcb11a9b2feb
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-31 14:36:15 -0300

    Install redux-thunk and set Provider with store in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import logo from './assets/planet.png';
 import Navbar from './components/Navbar';
 import './App.css';
 import Rockets from './components/Rockets';
 import Missions from './components/missions/Missions';
 import Profile from './components/profile/Profile';
+import getRockets from './redux/slices/rocketsSlice';
+import store from './redux/configureStore';
 
 function App() {
+  getRockets();
   return (
-    <div className="App">
-      <Router>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Navbar />
-        </header>
-        <Switch>
-          <Route exact path="/">
-            <Rockets />
-          </Route>
-          <Route path="/missions">
-            <Missions />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Navbar />
+          </header>
+          <Switch>
+            <Route exact path="/">
+              <Rockets />
+            </Route>
+            <Route path="/missions">
+              <Missions />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,24 @@
+import { GET_ROCKETS, GET_ROCKETS_SUCCESS, GET_ROCKETS_ERR } from '../slices/rocketsSlice';
+
+// Initial state
+
+const initialState = {
+  rockets: []
+}
+
+// Reducer 
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {  
+    case GET_ROCKETS:
+      return { ...state, pending: true };
+    case GET_ROCKETS_SUCCESS:
+      return { ...state, pending: false, rockets: action.rockets };
+    case GET_ROCKETS_ERR:
+      return { ...state, pending: false, error: action.error };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
