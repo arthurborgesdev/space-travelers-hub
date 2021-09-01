98a1c743e02b76dccc0a7f8d7fbaade6a69cb565
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-01 11:56:25 -0300

    Add more Styling to the buttons and notification in Rocket page

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -2,7 +2,7 @@ import { GET_ROCKETS, GET_ROCKETS_SUCCESS, GET_ROCKETS_ERR } from '../slices/roc
 
 // Actions
 
-const TOGGLE_RESERVE_ROCKET = 'rockets/rockets/RESERVE_ROCKET';
+const TOGGLE_RESERVE_ROCKET = 'rockets/rockets/TOGGLE_RESERVE_ROCKET';
 
 // Initial state
 
