a9485c1cc9e6ecc65d1abb636ab6aa54e4df686f
Arthur Borges <arthurborges.dev@gmail.com>
2021-09-01 12:35:14 -0300

    Add feature to list the reserved rockets on the My Profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>
-    <div>My Missions</div>
-    <div>My Rockets</div>
-  </div>
-);
+const Profile = () => {
+  const rockets = useSelector((state) => state.rocketsReducer.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
+  return (
+    <div className="profile-container">
+      <div className="missions-column">
+        <h2>My Missions</h2>
+      </div>
+      <div className="rockets-column">
+        <h2>My Rockets</h2>
+        {
+          reservedRockets.map((rocket) => (
+            <div key={rocket.id} className="rocket-name">{rocket.rocket_name}</div>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
